9fd6ebee8be6f028e0d3be250b49dd65fd5a5977
sireline <m.sireline@gmail.com>
2020-10-03 15:02:21 +0900

    new app

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -30,6 +29,7 @@ except ImportError:
     pass
 
 if not DEBUG:
+    import os
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
     django_heroku.settings(locals())
